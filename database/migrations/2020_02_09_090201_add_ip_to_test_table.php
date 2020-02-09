9bfcbfe32d05ec15f59923c8759ff259c314a88f
Shubham <shubhamkrverma73@gmail.com>
2020-02-09 14:47:16 +0530

    Added migration
    Added migration for test table just for test.

---
+++ database/migrations/2020_02_09_090201_add_ip_to_test_table.php
@@ -0,0 +1,42 @@
+<?php
+//https://stackoverflow.com/questions/16791613/add-a-new-column-to-existing-table-in-a-migration
+
+/*
+for Laravel 3:
+php artisan migrate:make add_paid_to_users
+
+for Laravel 5+:
+php artisan make:migration add_paid_to_users_table --table=users*/
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIpToTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('test', function (Blueprint $table) {
+            $table->string('ip')->after('message');
+            $table->string('browser')->after('ip');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('test', function (Blueprint $table) {
+            $table->dropColumn('ip');
+            $table->dropColumn('browser');
+        });
+    }
+}
