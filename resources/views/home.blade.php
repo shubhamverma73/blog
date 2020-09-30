637e55a93d249aa1e4c97fee3bcda8a3b88c50a6
Shubham <shubhamkrverma73@gmail.com>
2020-09-30 21:11:53 +0530

    U

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -80,7 +80,7 @@
 		  <div class="row justify-content-center mb-3 pb-3">
 			 <div class="col-md-12 heading-section text-center ftco-animate">
 				<span class="subheading">Featured Products</span>
-				<h2 class="mb-4">Our Products ...</h2>
+				<h2 class="mb-4">Our Products for You</h2>
 				<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
 			 </div>
 		  </div>
